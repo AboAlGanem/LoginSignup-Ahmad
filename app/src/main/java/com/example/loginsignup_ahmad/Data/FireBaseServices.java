a04c682e7dd25fdab029cdf267fd391602a1ecd1
AhmadGhneim <ahmadghneim21@gmail.com>
2025-05-18 14:16:25 +0300

    finish

--- app/src/main/java/com/example/loginsignup_ahmad/Data/FireBaseServices.java
+++ app/src/main/java/com/example/loginsignup_ahmad/Data/FireBaseServices.java
@@ -1,59 +1,38 @@
 package com.example.loginsignup_ahmad.Data;
-
 import android.net.Uri;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
-
-
 public class FireBaseServices {
-
     private static FireBaseServices instance;
     private FirebaseAuth auth;
     private FirebaseFirestore fire;
     private FirebaseStorage storage;
     private Uri selectedImageURL;
-
     public Uri getSelectedImageURL() {
         return selectedImageURL;
     }
-
     public void setSelectedImageURL(Uri selectedImageURL) {
-        this.selectedImageURL = selectedImageURL;
-    }
-
-
+        this.selectedImageURL = selectedImageURL;}
     public FireBaseServices()
-    {
-        auth=FirebaseAuth.getInstance();
+    {auth=FirebaseAuth.getInstance();
         fire=FirebaseFirestore.getInstance();
         storage=FirebaseStorage.getInstance();
-        selectedImageURL = null;
-    }
-
+        selectedImageURL = null;}
     public FirebaseStorage getStorage() {
         return storage;
     }
-
     public FirebaseFirestore getFire() {
         return fire;
     }
-
     public FirebaseAuth getAuth() {
         return auth;
     }
-
     public static FireBaseServices getInstance()
-    {
-        if(instance==null) {
-            instance = new FireBaseServices();
-        }
-        return instance;
+    {if(instance==null) {
+            instance = new FireBaseServices();}return instance;
     }
-
-
-
 }
 
 
